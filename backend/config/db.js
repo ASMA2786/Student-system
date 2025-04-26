e4a54e08f3b1b735622edaacd9c96cfecf6c9381
ASMA2786 <shaik.asmaaaa2786@gmail.com>
2025-04-26 14:24:39 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://asmashaik45638:OaDwhsIVIDRNc7Qy@cluster0.34xkjxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,,
     });
     console.log('MongoDB connected successfully');
   } catch (error) {
